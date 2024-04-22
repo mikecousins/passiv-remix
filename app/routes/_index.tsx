b67f97f8c06c6f52ff6e84ef50e9912477325013
Mike Cousins <mike@mikecousins.com>
2024-04-22 14:58:26 -0600

    Getting balances, positions, accuracy

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -10,38 +10,78 @@ import { getSession } from '~/services/session.server';
 type PortfolioGroup = {
   id: string;
   name: string;
+};
+
+type Balance = {
+  cash: number;
 }
+
+type Position = {
+  fractional_units: number;
+  price: number;
+}
+
+type PortfolioGroupInfo = {
+  accuracy: number;
+  balances: Balance[];
+  calculated_trades: {
+    id: string;
+    trades: unknown[];
+  }
+  positions: Position[];
+};
 
 export const meta: MetaFunction = () => {
   return [
     { title: 'Passiv' },
-    { name: 'description', content: 'Welcome to Remix!' },
   ];
 };
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   const session = await getSession(request.headers.get('Cookie'));
   const token = session.get('jwt_token');
-  const groups = await axios.get<PortfolioGroup[]>(
-    'portfolioGroups',
-    {
-      headers: {
-        Authorization: `JWT ${token}`,
-      },
+  const groups = await axios.get<PortfolioGroup[]>('portfolioGroups', {
+    headers: {
+      Authorization: `JWT ${token}`,
     },
-  );
+  });
+  const fullGroups = [];
+  let totalEquity = 0;
+  for (let i = 0; i < groups.data.length; i++) {
+    const response = await axios.get<PortfolioGroupInfo>(
+      `portfolioGroups/${groups.data[i].id}/info/`,
+      {
+        headers: {
+          Authorization: `JWT ${token}`,
+        },
+      },
+    );
+    let cash = 0;
+    response.data.balances.forEach((balance) => {
+      cash += balance.cash;
+    });
+    let equity = 0;
+    response.data.positions.forEach((position) => {
+      equity += position.fractional_units * position.price;
+    });
+    fullGroups.push({ ...response.data, ...groups.data[i], cash, equity });
+    totalEquity += cash + equity;
+  }
   return json({
-    groups: groups.data,
+    groups: fullGroups,
+    totalEquity,
   });
 };
 
 export default function Index() {
-  const data = useLoaderData<typeof loader>();
+  const { groups, totalEquity } = useLoaderData<typeof loader>();
+  console.log(groups[0].positions);
   return (
     <div className="container mx-auto max-w-7xl">
       <h1 className="text-red-500 font-bold">Passiv</h1>
-      {data.groups.map((group) => (
-        <div key={group.id}>{group.name}</div>
+      <div>Total Equity: {totalEquity}</div>
+      {groups.map((group) => (
+        <div key={group.id}>{group.name} {group.accuracy}% ${group.cash} ${group.equity}</div>
       ))}
     </div>
   );
