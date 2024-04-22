765c139ccab69b7d9d61df170a0b6992a8d455ca
Mike Cousins <mike@mikecousins.com>
2024-04-22 11:25:14 -0600

    Adding logout route

---
+++ app/routes/logout.tsx
@@ -0,0 +1,10 @@
+import type { ActionFunctionArgs } from '@remix-run/node';
+import { authenticator } from '~/services/auth.server';
+
+export async function action({ request }: ActionFunctionArgs) {
+  await authenticator.logout(request, { redirectTo: '/login' });
+}
+
+export default function Logout() {
+  return null;
+}
